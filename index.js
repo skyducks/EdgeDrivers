957a843644c471e6a3535b5082adcf5c2eb9d644
w35l3y <w35l3y@brasnet.org>
2024-06-10 23:05:18 -0300

    .

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const { update_models_zigbee } = require("./helpers");
 
 console.log(
   "process.env",
-  process.env.WSL,
+  process.env.WSL_1,
+  process.env.WSL_2,
+  process.env.WSL_3,
+  process.env.WSL_4,
+  process.env.WSL_5,
   process.env.DRIVER_FOLDER,
   process.env.DRIVER_VERSION
 );
