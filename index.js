718f203b87d4703c54d4928422faf446946be363
w35l3y <w35l3y@brasnet.org>
2024-06-10 22:51:28 -0300

    Updated environment variable

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const { update_models_zigbee } = require("./helpers");
 
-//console.log(process.env.DRIVER_FOLDER, process.env.DRIVER_VERSION)
+console.log(
+  "process.env",
+  process.env.DRIVER_FOLDER,
+  process.env.DRIVER_VERSION
+);
 update_models_zigbee(process.env.DRIVER_FOLDER, process.env.DRIVER_VERSION);
 // update_models_lan("event-stream", ({ service_type, domain }) =>
 //   [...service_type.split("."), domain].join("/")
