54d468ac6fdc29f1b8a6f113eaffa842198d1681
w35l3y <w35l3y@brasnet.org>
2024-06-15 13:24:52 -0300

    .

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 const { update_models_zigbee } = require("./helpers");
 
-console.log(
-  "dotenv",
-  require("dotenv").config({
-    //path: process.env.GITHUB_ENV,
-    debug: true,
-    encoding: "utf8",
-  })
-);
-
-console.log("process.env", process.env);
 update_models_zigbee(process.env.DRIVER_FOLDER, process.env.DRIVER_VERSION);
 // update_models_lan("event-stream", ({ service_type, domain }) =>
 //   [...service_type.split("."), domain].join("/")
