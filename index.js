9e542df3acf4912bd352fc73330941cb11bfbcd9
w35l3y <w35l3y@brasnet.org>
2024-06-15 08:07:34 -0300

    Added dotenv

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { update_models_zigbee } = require("./helpers");
 
-console.log(
-  "process.env",
-  process.env
-);
+require("dotenv").config({
+  path: process.env.GITHUB_ENV,
+  debug: true,
+});
+
+console.log("process.env", process.env);
 update_models_zigbee(process.env.DRIVER_FOLDER, process.env.DRIVER_VERSION);
 // update_models_lan("event-stream", ({ service_type, domain }) =>
 //   [...service_type.split("."), domain].join("/")
